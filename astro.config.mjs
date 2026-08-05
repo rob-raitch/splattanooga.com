a2817167a112ab4129bdfdd6a5b0c1682bdc07d0
Rob Aitchison <raitch@pm.me>
2026-08-05 16:44:13 -0400

    Fixes from Turtle Wolfe feedback

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,4 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 export default defineConfig({
   site: 'https://splattanooga.com',
-  // Move "fonts" completely out of the experimental block!
-  fonts: [
-    {
-      provider: fontProviders.google(),
-      name: 'Flavors',
-      cssVariable: '--font-flavors',
-    },
-    {
-      provider: fontProviders.google(),
-      name: 'Quicksand',
-      cssVariable: '--font-quicksand', // Create a separate variable for your body text
-    }
-  ]
 });
